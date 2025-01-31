4fd5a35d1888253468464bf326c47c028f15d577
SURAN SINGH DHAMI <154992014+Dhammi7@users.noreply.github.com>
2025-01-31 11:57:39 +0530

    Update Promotion.js

--- src/components/feedComponents/Promotion.js
+++ src/components/feedComponents/Promotion.js
@@ -6,7 +6,7 @@ const Promotion = () => {
 
   const doSubscribe = async () => {
     try {
-      const response = await fetch('http://localhost:8000/subscribe', {
+      const response = await fetch('mongodb+srv://dhami:suran@cluster0.10w2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
