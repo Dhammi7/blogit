3b91a2c94706c15bfe9ac8e8964795bdfa88906c
SURAN SINGH DHAMI <154992014+Dhammi7@users.noreply.github.com>
2025-01-31 12:37:37 +0530

    Update login.js

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -16,7 +16,7 @@ const Login = () => {
     setSuccessMessage('');
 
     try {
-      const response = await fetch('mongodb+srv://dhami:suran@cluster0.10w2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      const response = await fetch('https://blogit-backend-mxsw.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
